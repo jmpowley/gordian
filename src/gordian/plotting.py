b4ec2e53cd57ae21b04374fa7f34f4a87058202e
jmpowley <jonahpowley1@gmail.com>
2026-01-16 15:13:42 +0000

    Tidy file

--- src/gordian/plotting.py
+++ src/gordian/plotting.py
@@ -11,18 +11,19 @@ from pysersic.rendering import HybridRenderer
 
 def make_plots(fitter, image, mask, sig, psf, profile_type, method, filter, fig_dir):
 
-    # Make plots
-    # -- data
+    # Plot input data 
     fig = call_plot_image(image, mask, sig, psf)
     fig_name = f"{profile_type}_{method}_{filter}_data.pdf"
     fig.savefig(os.path.join(fig_dir, fig_name))
     plt.close(fig)
-    # -- residual
+
+    # Plot model comparison
     fig = call_plot_residual(fitter, image, mask, psf, profile_type)
     fig_name = f"{profile_type}_{method}_{filter}_residual.pdf"
     fig.savefig(os.path.join(fig_dir, fig_name))
     plt.close(fig)
-    # -- corner
+    
+    # Plot posteriors
     fig = fitter.sampling_results.corner(color='C0')
     fig_name = f"{profile_type}_{method}_{filter}_corner.pdf"
     fig.savefig(os.path.join(fig_dir, fig_name))
@@ -30,22 +31,24 @@ def make_plots(fitter, image, mask, sig, psf, profile_type, method, filter, fig_
 
 def call_plot_residual(fitter, im, mask, psf, profile_type):
 
-    # Generate best fit model
+    # Generate best model
     summary = fitter.sampling_results.summary()
     dict = {}
     for hdul, b in zip(summary.index, summary["mean"]):
         dict[hdul] = b
     best_model = HybridRenderer(im.shape, jnp.array(psf.astype(np.float32))).render_source(dict, profile_type=profile_type)
     
+    # Call plot_residual
     fig, ax = plot_residual(im, best_model, mask=mask, vmin=-1, vmax=1)
+    fig.tight_layout()
 
     return fig
 
 def call_plot_image(image, mask, sig, psf):
 
+    # Call plot_image
     fig, ax = plot_image(image, mask, sig, psf)
-
-    plt.tight_layout()
+    fig.tight_layout()
 
     return fig
 
@@ -61,7 +64,6 @@ def plot_data_grid(trees, filters):
         ax = axes[i, :]
 
         # Load data
-        # -- image data
         input_data = tree["input_data"]
         image = np.asarray(input_data["image"])
         mask = np.asarray(input_data["mask"])
@@ -85,21 +87,23 @@ def plot_residuals_grid(images, masks, models, residuals, filters : str, scale :
         image_masked[mask.astype(bool)] = np.nan
         model_masked[mask.astype(bool)] = np.nan
 
-        # Make plot
-        # -- scale limits
+        # Scale plot limits
         vmin = np.nanmean(image) - (scale * np.nanstd(image, ddof=1))
         vmax = np.nanmean(image) + (scale * np.nanstd(image, ddof=1))
-        # -- image
+        
+        # Plot data
         ax[0].imshow(image_masked, origin="lower", cmap="gray_r", vmin=vmin, vmax=vmax)
         ax[0].text(0.03, 0.97, f"{filter.upper()}", transform=ax[0].transAxes, va="top", ha="left", size=14)
-        # -- model
+
+        # Plot model
         ax[1].imshow(model_masked, origin="lower", cmap="gray_r", vmin=vmin, vmax=vmax)
-        # -- residual
+        
+        # Plot residual
         im_resid = ax[2].imshow(residual, origin="lower", cmap="seismic", vmin=resid_vmin, vmax=resid_vmax)
         ax_divider = make_axes_locatable(ax[2])
         cax = ax_divider.append_axes("right", size="7%", pad="2%")
         cb = fig.colorbar(im_resid, cax=cax)
 
-    plt.tight_layout()
+    fig.tight_layout()
 
     return fig
