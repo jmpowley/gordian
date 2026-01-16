b1c129d3bdd29b66eec1cc8440edd4ca61928882
jmpowley <jonahpowley1@gmail.com>
2026-01-16 15:09:38 +0000

    Streamline runner file

--- src/gordian/gordian.py
+++ src/gordian/gordian.py
@@ -1,16 +1,17 @@
 """Main Gordian class for IFU bulge-disc decomposition."""
+from typing import Union
 
 from .config import GordianConfig, load_config_from_dict
 from .fitting import fit_bands_independent, fit_bands_simultaneous, fit_cube
-from .writing import save_simultaneous_fit_results_to_asdf, create_directory
+from .writing import save_simultaneous_fit_results, create_directory
 
 
 class Gordian:
-    """Performs bulge-disc decomposition of IFU cubes"""
+    """Base class for the Gordian package."""
 
-    def __init__(self, config: dict):
+    def __init__(self, config: Union[dict, GordianConfig]):
         """
-        Initialize Gordian pipeline.
+        Initialize Gordian.
         
         Parameters
         ----------
@@ -20,65 +21,68 @@ class Gordian:
         print("Gordian initialised")
         
         # Convert dict to dataclass
-        if isinstance(config, dict):
+        if isinstance(config, GordianConfig):
+            self.config = config
+        elif isinstance(config, dict):
             self.config: GordianConfig = load_config_from_dict(config)
         else:
-            self.config = config
+            raise Exception(f"Invalid config type: {type(config)}")
 
-    def fit_bands(self):
-        """Fit photometric bands to obtain structural parameters"""
+    def run_fit_bands(self):
+        """Runs the fitting of photometric bands.
+
+        Depending on the `fit_type` in the configuration, it either fits
+        each band independently or performs a simultaneous fit across all bands.
+
+        Raises
+        ------
+        ValueError
+            If `fit_type` is not 'independent' or 'simultaneous'.
+        """
         
         fit_type = self.config.fit_config.fit_type
-        
-        if fit_type == "independent":
-            return fit_bands_independent(
-                band_config=self.config.band_config,
-                prior_config=self.config.prior_config,
-                fit_config=self.config.fit_config,
-                io_config=self.config.io_config,
-            )
-        elif fit_type == "simultaneous":
-            return fit_bands_simultaneous(
-                band_config=self.config.band_config,
-                cube_config=self.config.cube_config,
-                prior_config=self.config.prior_config,
-                fit_config=self.config.fit_config,
-                io_config=self.config.io_config,
-            )
-        else:
-            raise ValueError(f"fit_type must be 'independent' or 'simultaneous', got {fit_type}")
 
-    def fit_cube(self, results_dict):
-        """Fit cube slices using structural parameters from fit"""
+        if fit_type == "independent":
+            # Run independent fits for each band
+            fit_bands_independent(
+                band_config=self.config.band_config,
+                model_config=self.config.model_config,
+                fit_config=self.config.fit_config,
+                io_config=self.config.io_config,
+            )
         
-        if self.config.cube_config is None:
-            print("No cube configuration provided. Skipping cube fitting.")
-            return
+        elif fit_type == "simultaneous":
+            # Run a simultaneous fit across all bands and store the results
+            self.bands_results_dict = fit_bands_simultaneous(
+                band_config_dict=self.config.band_config,
+                cube_config=self.config.cube_config,
+                model_config=self.config.model_config,
+                fit_config=self.config.fit_config,
+                io_config=self.config.io_config,
+            )
+            
+        else:
+            raise ValueError(
+                f"fit_type must be 'independent' or 'simultaneous', got {fit_type}"
+            )
+
+    def run_fit_cube(self, results_dict):
+        """Runs the fitting of the IFU cube."""
         
         fit_cube(
             cube_config=self.config.cube_config,
             results_dict=results_dict,
             fit_config=self.config.fit_config,
-            prior_config=self.config.prior_config,
+            model_config=self.config.model_config,
+            io_config=self.config.io_config,
         )
 
     def run(self):
-        """Wrapper for Gordian. Prepares data for fitting and then runs pipeline"""
-        
-        # Initialise directories
-        create_directory(self.config.io_config.fig_dir)
-        create_directory(self.config.io_config.out_dir)
+        """Wrapper for running Gordian."""
 
-        # Fit photometric bands to obtain structural parameters
-        bands_results_dict = self.fit_bands()
+        # Fit photometric bands
+        self.run_fit_bands()
         
-        # Save results for simultaneous fits
-        if self.config.fit_config.fit_type == "simultaneous":
-            save_simultaneous_fit_results_to_asdf(
-                results_dict=bands_results_dict,
-                config=self.config,
-            )
-        
-        # Extract structural components from cube if provided
+        # Fit cube if provided
         if self.config.cube_config is not None:
-            self.fit_cube(results_dict=bands_results_dict)
+            self.run_fit_cube()
