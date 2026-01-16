130cb52aa8f128e8e41988395e07f120aca5436a
jmpowley <jonahpowley1@gmail.com>
2026-01-16 15:08:41 +0000

    Save simultaneous fit results into fit_bands_simultaneous

--- src/gordian/fitting.py
+++ src/gordian/fitting.py
@@ -2,9 +2,9 @@ import os
 from typing import Dict, Optional
 
 import numpy as np
-from sedpy.observate import load_filters
-
 import matplotlib.pyplot as plt
+
+from sedpy.observate import load_filters
 
 import jax
 import jax.numpy as jnp
@@ -12,39 +12,53 @@ from jax.random import PRNGKey
 
 from pysersic import FitSingle
 from pysersic.multiband import FitMultiBandPoly, FitMultiBandBSpline
-from pysersic.priors import autoprior
+from pysersic.priors import autoprior, PySersicSourcePrior
 from pysersic import loss
 
-from .config import BandConfig, CubeConfig, PriorConfig, FitConfig, IOConfig
+from .config import BandConfig, CubeConfig, ModelConfig, ModelConfig, IOConfig
 from .loading import load_band_data, load_cube_data
-from .plotting import call_plot_residual, call_plot_image, make_plots
+from .plotting import make_plots
+from .writing import save_individual_fit_result, save_simultaneous_fit_results
 from .helpers import (
     return_linked_param_at_wv, 
     return_const_param, 
     return_MAP_from_fitter, 
-    return_summary_from_estimation
+    return_summary_from_fitter
 )
 
 
-def _set_priors(image, mask, prior_config: PriorConfig):
-    """Sets priors for model in PySersic"""
+def set_priors(image: np.ndarray, mask: np.ndarray, model_config: ModelConfig):
+    """Sets priors for model in PySersic.
+
+    Parameters
+    ----------
+    image : np.ndarray
+        Input image data
+    mask : np.ndarray
+        Input mask data
+    model_config : ModelConfig
+        Configuration for a model parameters
+
+    Returns
+    -------
+    prior : pysersic.priors.PySersicSourcePrior
+        Prior to be incorporated into PySersic fit
+    """
     
     # Generate priors from image
     prior = autoprior(
         image=image, 
-        profile_type=prior_config.profile_type, 
+        profile_type=model_config.profile_type, 
         mask=mask, 
-        sky_type=prior_config.sky_type
+        sky_type=model_config.sky_type, 
     )
 
     # Set priors from config dict
-    for prior_type, prior_type_dict in prior_config.prior_dict.items():
-        # -- uniform priors
+    for prior_type, prior_type_dict in model_config.prior_dict.items():
         if prior_type == "uniform":
             for param, range in prior_type_dict.items():
                 lo, hi = range
                 prior.set_uniform_prior(param, lo, hi)
-        # -- gaussian priors
         if prior_type == "gaussian":
             for param, gauss in prior_type_dict.items():
                 loc, std = gauss
@@ -53,12 +67,23 @@ def _set_priors(image, mask, prior_config: PriorConfig):
     return prior
 
 
-def fit_single(data, mask, rms, psf, prior, loss_func, method, rkey, verbose):
-    """Fits single image. Either photometric band or slice/set of slices from IFU cube"""
+def fit_single(
+        data: np.ndarray, 
+        mask: np.ndarray, 
+        rms: np.ndarray, 
+        psf: np.ndarray, 
+        prior, 
+        loss_func: str, 
+        method: str, 
+        rkey, 
+        verbose: bool,
+        ):
+    """Fits single image. Either a photometric band or a slice from an IFU cube."""
 
     if verbose:
         print("Starting fit...")
 
+    # Map for available loss functions
     loss_map = {
         "student_t_loss": loss.student_t_loss,
         "student_t_loss_free_sys": loss.student_t_loss_free_sys,
@@ -67,16 +92,14 @@ def fit_single(data, mask, rms, psf, prior, loss_func, method, rkey, verbose):
     }
     loss_func = loss_map[loss_func]
 
-    # Setup fitter
+    # Run fit
     fitter = FitSingle(data=data, rms=rms, mask=mask, psf=psf, prior=prior, loss_func=loss_func)
 
-    # Estimate posterior
+    # Sample posterior
     rkey, rkey_est = jax.random.split(rkey, 2)
-    # -- MCMC sampling
     if method == "mcmc":
         fitter.sample(rkey=rkey_est)
         result = fitter.sampling_results
-    # -- SVI
     elif method == "svi":
         result = fitter.estimate_posterior(rkey=rkey_est)
     
@@ -90,11 +113,28 @@ def fit_single(data, mask, rms, psf, prior, loss_func, method, rkey, verbose):
 
 def fit_bands_independent(
     band_config: Dict[str, BandConfig],
-    prior_config: PriorConfig,
-    fit_config: FitConfig,
+    model_config: ModelConfig,
+    fit_config: ModelConfig,
     io_config: IOConfig,
 ):
-    """Fit bands independently"""
+    """Fits set of photometric bands independently of each other.
+
+    Parameters
+    ----------
+    band_config : Dict[str, BandConfig]
+        Configuration for a single photometric band
+    model_config : ModelConfig
+        Configuration for a model parameters
+    fit_config : FitConfig
+        Configuration for a fitting
+    io_config : IOConfig
+        Configuration for a data input/output
+
+    Returns
+    -------
+    _type_
+        _description_
+    """
     
     rkey = PRNGKey(fit_config.seed)
 
@@ -102,7 +142,6 @@ def fit_bands_independent(
     for band_name, band_config in band_config.items():
 
         filter = band_config.filter
-        sedpy_filter = load_filters(["jwst_" + filter])[0]
         if fit_config.verbose:
             print("------------------------------")
             print(f"Filter: {filter.upper()}")
@@ -111,7 +150,7 @@ def fit_bands_independent(
         image, mask, sig, psf = load_band_data(band_config)
 
         # Set priors
-        prior = _set_priors(image=image, mask=mask, prior_config=prior_config)
+        prior = set_priors(image=image, mask=mask, model_config=model_config)
 
         # Fit band
         rkey, rkey_fit = jax.random.split(rkey)
@@ -127,52 +166,52 @@ def fit_bands_independent(
             verbose=fit_config.verbose
         )
 
-        # Save results
-        out_name = f"{prior_config.profile_type}_{fit_config.method}_fit_{filter}.asdf"
-        out_path = os.path.join(io_config.out_dir, out_name)
-        result.save_result(out_path)
-
         # Make plots
         make_plots(
             fitter, image, mask, sig, psf, 
-            profile_type=prior_config.profile_type, 
+            profile_type=model_config.profile_type, 
             method=fit_config.method, 
             filter=filter, 
-            fig_dir=io_config.fig_dir
-        )
+            fig_dir=io_config.fig_dir, 
+            )
 
-    return fitter, result
-
+        # Save results
+        save_individual_fit_result(result=result, 
+                                   model_config=model_config, 
+                                   fit_config=fit_config, 
+                                   io_config=io_config, 
+                                   )
 
 def fit_bands_simultaneous(
-    band_config: Dict[str, BandConfig],
+    band_config_dict: Dict[str, BandConfig],
     cube_config: Optional[CubeConfig],
-    prior_config: PriorConfig,
-    fit_config: FitConfig,
+    model_config: ModelConfig,
+    fit_config: ModelConfig,
     io_config: IOConfig,
 ):
     """Fit bands simultaneously with wavelength-dependent parameters"""
-    
+
+    # Initialise key
     rkey = PRNGKey(fit_config.seed)
 
+    # Initialise lists/dicts
     filters = []
     waveffs = []
-
     im_dict = {}
     mask_dict = {}
     rms_dict = {}
     psf_dict = {}
-    pysersic_prior_dict = {}
-
     ind_fitter_dict = {}
     ind_results_dict = {}
     sim_results_dict = {}
 
     # Loop over each band
-    for band_name, band_config in band_config.items():
+    for band_name, band_config in band_config_dict.items():
 
+        # Load filter
         filter = band_config.filter
         sedpy_filter = load_filters(["jwst_" + filter])[0]
+        
         if fit_config.verbose:
             print("------------------------------")
             print(f"Filter: {filter.upper()}")
@@ -181,7 +220,7 @@ def fit_bands_simultaneous(
         image, mask, sig, psf = load_band_data(band_config)
 
         # Create PySersic priors
-        prior = _set_priors(image=image, mask=mask, prior_config=prior_config)
+        prior = set_priors(image=image, mask=mask, model_config=model_config)
         if fit_config.verbose:
             print(prior)
 
@@ -200,14 +239,14 @@ def fit_bands_simultaneous(
         )
 
         # Save results
-        out_name = f"{prior_config.profile_type}_{fit_config.method}_fit_{filter}.asdf"
+        out_name = f"{model_config.profile_type}_{fit_config.method}_fit_{filter}.asdf"
         out_path = os.path.join(io_config.out_dir, out_name)
         result.save_result(out_path)
 
         # Make plots
         make_plots(
             fitter, image, mask, sig, psf, 
-            profile_type=prior_config.profile_type, 
+            profile_type=model_config.profile_type, 
             method=fit_config.method, 
             filter=filter, 
             fig_dir=io_config.fig_dir
@@ -216,16 +255,14 @@ def fit_bands_simultaneous(
         # Save to lists/dicts
         filters.append(filter)
         waveffs.append(sedpy_filter.wave_effective / 1e4)
-
         im_dict[filter] = image
         mask_dict[filter] = mask
         rms_dict[filter] = sig
         psf_dict[filter] = psf
-        pysersic_prior_dict[filter] = prior
         ind_fitter_dict[filter] = fitter
         ind_results_dict[filter] = result
 
-    # Create fine grid of wavelengths
+    # Create wavelength vectors to save
     waveffs = np.asarray(waveffs)
     if fit_config.use_cube_wave and cube_config is not None:
         wave, _, _, _ = load_cube_data(cube_config)
@@ -233,7 +270,7 @@ def fit_bands_simultaneous(
     else:
         wv_to_save = np.linspace(min(waveffs), max(waveffs), num=50)
     
-    # Invert wavelengths if requested
+    # Optionally invert wavelengths
     if fit_config.invert_wave:
         waveffs = 1 / waveffs
         wv_to_save = 1 / wv_to_save
@@ -260,20 +297,29 @@ def fit_bands_simultaneous(
             **fit_config.multifitter_kwargs,
         )
     
-    # Add multifit results
-    sim_results_dict["summary"] = return_summary_from_estimation(MultiFitter, rkey, fit_config.method)
+    # Add multifitter results
+    sim_results_dict["summary"] = return_summary_from_fitter(MultiFitter, rkey, fit_config.method)
     sim_results_dict["map"] = return_MAP_from_fitter(MultiFitter, rkey)
     sim_results_dict["wv_to_save"] = wv_to_save
     sim_results_dict["waveffs"] = waveffs
 
+    # Save results
+    save_simultaneous_fit_results(
+        results_dict=sim_results_dict, 
+        band_config_dict=band_config_dict, 
+        cube_config=cube_config, 
+        model_config=model_config,
+        fit_config=fit_config,
+        io_config=io_config,
+        )
+    
     return sim_results_dict
-
 
 def fit_cube(
     cube_config: CubeConfig,
     results_dict: Dict,
-    fit_config: FitConfig,
-    prior_config: PriorConfig,
+    fit_config: ModelConfig,
+    model_config: ModelConfig,
 ):
     """Fit IFU cube slices using parameters from band fits"""
     
@@ -303,7 +349,7 @@ def fit_cube(
         # Build Gaussian priors for this slice using medians and stds
         slice_gaussian_priors = {}
         for param, (meds, stds) in linked_params_dict.items():
-            if prior_config.profile_type == "sersic_exp" and param != "f_1":
+            if model_config.profile_type == "sersic_exp" and param != "f_1":
                 slice_gaussian_priors[param] = (meds[i], stds[i])
         
         slice_const_priors = {}
@@ -317,14 +363,14 @@ def fit_cube(
         }
         
         # Create prior config for this slice
-        slice_prior_config = PriorConfig(
-            profile_type=prior_config.profile_type,
-            sky_type=prior_config.sky_type,
+        slice_prior_config = ModelConfig(
+            profile_type=model_config.profile_type,
+            sky_type=model_config.sky_type,
             prior_dict=slice_prior_dict
         )
         
         # Set priors
-        slice_prior = _set_priors(image=slice, mask=mask, prior_config=slice_prior_config)
+        slice_prior = set_priors(image=slice, mask=mask, model_config=slice_prior_config)
 
         # Fit individual slice
         rkey, rkey_fit = jax.random.split(rkey)
